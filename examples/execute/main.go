fe520e7f2c75ed209b6da41bbfbe7ee95257a3b7
Nahuel <nlcostamagna@gmail.com>
2022-03-16 15:41:43 -0300

    feat-001: get & save example

--- examples/execute/main.go
+++ examples/execute/main.go
@@ -1,29 +1,64 @@
 package main
 
 import (
-	"github.com/aws/aws-sdk-go/aws/credentials"
-
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/digitalhouse-dev/dynamo/dynamo"
+	"os"
+	"time"
 )
+
+const dynamoAccessKey = "[Your access key]"
+const dynamoSecretKey = "[Your secret key]"
+const dynamoToken = "[Your token key]"
+const dynamoEndpoint = "[Your dynamo endpoint]"
+const awsRegion = "[Your aws region]"
+const table = "[Your dynamo table]"
 
 func main() {
 
-	dyn := CreateDynamoDBClient("[Your region]")
+	dyn, err := CreateDynamoDBClient(awsRegion)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
 
+	// Get a value
+	{
+		query := dynamo.Query(table, dynamo.QueryType, "", dyn)
+		query = query.Criterial("resource_id", dynamo.Equals, "myResourceTest").Order(dynamo.ASC)
+		res, key, err := query.Execute()
+		fmt.Println(res, key, err)
+	}
+
+	// Save a value
+	{
+		t := time.Now()
+		myDomain := struct {
+			ResourceID string     `json:"resource_id"`
+			CreatedAt  *time.Time `json:"created_at"`
+			ID         string     `json:"id"`
+			Note       string     `json:"note"`
+			WrittenBy  string     `json:"written_by"`
+		}{
+			"myResourceTest", &t, "123", "Test", "ncostamagna@digitalhouse.com",
+		}
+		err := dynamo.Save(table, dyn).Entity(myDomain).Execute()
+		fmt.Println(err)
+	}
 }
 
 func CreateDynamoDBClient(region string) (*dynamodb.DynamoDB, error) {
 	conf := aws.NewConfig().WithRegion(region)
 
-	dynamoAccessKey := "[Your ACCESS_KEY_ID]"
-	dynamoSecretKey := "[Your SECRET_ACCESS_KEY]"
 	if dynamoAccessKey != "" && dynamoSecretKey != "" {
-		cred := credentials.NewStaticCredentials(dynamoAccessKey, dynamoSecretKey, "")
+		cred := credentials.NewStaticCredentials(dynamoAccessKey, dynamoSecretKey, dynamoToken)
 		conf = conf.WithCredentials(cred)
 	}
-	dynamoEndpoint := "[Your Dynamo Endpoint]"
+
 	if dynamoEndpoint != "" {
 		conf = conf.WithEndpoint(dynamoEndpoint)
 	}
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/digitalhouse-dev/dynamo
 
-go 1.17
+go 1.16
 
-require github.com/aws/aws-sdk-go v1.43.18 // indirect
+require github.com/aws/aws-sdk-go v1.43.5
